dbe62a4fe1f72a4c66e0dadfc977b4500c838648
Oyun <1340839769@qq.com>
2024-07-25 12:58:20 +0800

    feat: Generate the test data

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,19 @@
-use poseidon::Poseidon;
-use halo2curves::{bn256::Fr, ff::Field};
+use halo2curves::bn256::Fq;
+use halo2curves::{bn256::{Fr, G1Affine}, ff::Field};
 use rand_core::OsRng;
 use serde::Serialize;
 use serde_json::json;
 use hex::encode; 
-use halo2_base::halo2_proofs::halo2curves::bn256::G1Affine;
-
 use std::fs::File;
-use std::io::Write;
+use std::io::{Read, Write};
 use std::time::Instant;
+use poseidon::Poseidon;
 
 #[derive(Serialize)]
 struct MerklePath {
-    input: String,
+    sk: String,
+    pk_x: String,
+    pk_y: String,
     path: Vec<String>,
     index: Vec<bool>,
 }
@@ -22,25 +23,51 @@ fn fr_to_string(fr: &Fr) -> String {
     encode(bytes)
 }
 
-fn main() {
 
+fn fq_to_string(fq : &Fq) -> String {
+    let bytes = fq.to_bytes();
+    encode(bytes)
+}
+
+fn hash_x_coordinate(pk: &G1Affine) -> Fr {
+    let x_bytes = pk.x.to_bytes();
+    // println!("x_bytes: {:?}", fr_to_string(&Fr::from_bytes(&x_bytes).unwrap()));
+    let mut hasher = Poseidon::<Fr, 3, 2>::new(8, 57);
+    hasher.update(&[Fr::from_bytes(&x_bytes).unwrap(  )]);
+    hasher.squeeze()
+}
+
+fn main() {
+    // 从data/merkle_depth.in文件中读取输入
+    let mut file = File::open("data/merkle_depth.in").expect("Unable to open file");
+    let mut data = String::new();
+    file.read_to_string( &mut data).expect("Unable to read file");
+    let merkle_depth: usize = data.trim().parse().expect("Invalid input");
+
+    // k = 2^depth
+    let k = 1 << merkle_depth;
     let inputs_start = Instant::now();
-    // 生成1024个随机输入
-    let inputs: Vec<Fr> = (0..16).map(|_| Fr::random(OsRng)).collect();
+    // 生成k个随机输入
+    let mut sk_pks = Vec::new();
+    for _ in 0..k {
+        let sk = Fr::random(OsRng);
+        let pubkey = G1Affine::from(G1Affine::generator() * sk); // Calculate public key from secret key
+        sk_pks.push((sk, pubkey));
+        // println!("sk: {:?}, pk: {:?}", sk, pubkey);
+    }
     let duration = inputs_start.elapsed();
-    println!("Time to generate inputs: {:?}", duration);
+    println!("Time to generate keys: {:?}", duration);
 
     let merkle_start = Instant::now();
     // 存储Merkle树的叶子节点
     let mut leaves = Vec::new();
-    for input in &inputs {
-        let mut hasher = Poseidon::<Fr, 3, 2>::new(8, 57);
-        hasher.update(&[*input]);
-        leaves.push(hasher.squeeze());
+    for (_, pk) in &sk_pks {
+        let hashed_pk = hash_x_coordinate(pk);
+        leaves.push(hashed_pk);
+        // println!("hashed_pk: {:?}", hashed_pk);
     }
     let duration = merkle_start.elapsed();
     println!("Time to generate leaves: {:?}", duration);
-
 
     // 构建Merkle树并记录路径
     let mut paths = Vec::new();
@@ -65,7 +92,7 @@ fn main() {
     let root = tree.last().unwrap()[0];
 
     let start = Instant::now();
-    for (i, input) in inputs.iter().enumerate() {
+    for (i, (sk, pk)) in sk_pks.iter().enumerate() {
         let mut path = Vec::new();
         let mut index = Vec::new();
         let mut position = i;
@@ -78,7 +105,9 @@ fn main() {
         }
 
         paths.push(MerklePath {
-            input: fr_to_string(input),
+            sk: fr_to_string(sk),
+            pk_x: fq_to_string(&pk.x),
+            pk_y: fq_to_string(&pk.y),
             path: path.iter().map(fr_to_string).collect(),
             index,
         });
@@ -91,11 +120,10 @@ fn main() {
         "root": fr_to_string(&root),
         "leaves": paths,
     })).unwrap();
-
-        // 将结果写入文件
-    let mut file = File::create("merkle_tree_16.json").expect("Unable to create file");
+    let path = "data/merkle_tree_from_g1_{k}.json".replace("{k}", &k.to_string());
+    // 将结果写入文件
+    let mut file = File::create(path).expect("Unable to create file");
     file.write_all(result.as_bytes()).expect("Unable to write data");
 
-    println!("Data has been written to merkle_tree.json");
-//     println!("{}", result);
+    println!("Data has been written to data/merkle_tree_from_g1_{}.json",k);
 }
